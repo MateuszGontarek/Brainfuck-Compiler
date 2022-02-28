22cce28d94885dbc77da603290d82c5e9ff57d32
MateuszGontarek <92920318+MateuszGontarek@users.noreply.github.com>
2022-02-28 12:29:18 +0100

    deleted oop

--- Brainfuck Compiler 2.py
+++ Brainfuck Compiler 2.py
@@ -1,28 +1,23 @@
-class nie:
-    def __init__(self):
-        self.tab = ["+","-",">",'<',".",",","[","]"]
-        self.indeks = [0] * 10000
-        self.Indeks = 0
-    def funkcja(self):
-        text = input()
-        i = 0
-        while i < len(text):
-            mark = text[i]
-            if not mark in self.tab: mark = ""
-            elif mark == "+": self.indeks[self.Indeks] += 1
-            elif mark == "-": self.indeks[self.Indeks] -= 1
-            elif mark == ">": self.Indeks += 1
-            elif mark == "<": self.Indeks -= 1
-            elif mark == ",": self.indeks[self.Indeks] += input()
-            elif mark == ".": print(chr(self.indeks[self.Indeks]), end="")
-            elif mark == "[": 
-                if self.indeks[self.Indeks] == 0:
-                    while text[i] != "]":
-                        i += 1
-            elif mark == "]": 
-                if self.indeks[self.Indeks] != 0:
-                    while text[i] != "[":
-                        i -= 1
-            i += 1
-n = nie()
-n.funkcja()
+tab = ["+","-",">",'<',".",",","[","]"]
+indeks = [0] * 10000
+Indeks = 0
+text = input()
+i = 0
+while i < len(text):
+    mark = text[i]
+    if not mark in self.tab: mark = ""
+    elif mark == "+": indeks[Indeks] += 1
+    elif mark == "-": indeks[Indeks] -= 1
+    elif mark == ">": Indeks += 1
+    elif mark == "<": Indeks -= 1
+    elif mark == ",": indeks[Indeks] += input()
+    elif mark == ".": print(chr(indeks[Indeks]), end="")
+    elif mark == "[": 
+        if indeks[Indeks] == 0:
+            while text[i] != "]":
+                i += 1
+    elif mark == "]": 
+        if indeks[Indeks] != 0:
+            while text[i] != "[":
+                i -= 1
+    i += 1
